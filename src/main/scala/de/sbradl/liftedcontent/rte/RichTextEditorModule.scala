484945d7f8ff595a62e6faaf2ee7073d4953f9f2
Stefan Bradl <stefan_bradl@web.de>
2012-02-13 11:41:43 +0100

    using directory toserve for additional content

--- src/main/scala/de/sbradl/liftedcontent/rte/RichTextEditorModule.scala
+++ src/main/scala/de/sbradl/liftedcontent/rte/RichTextEditorModule.scala
@@ -4,10 +4,17 @@ import net.liftweb.sitemap.LocPath.stringToLocPath
 import net.liftweb.sitemap.Loc.Hidden
 import net.liftweb.sitemap.Menu
 import de.sbradl.liftedcontent.util.Module
+import net.liftweb.http.ResourceServer
 
 class RichTextEditorModule extends Module {
 
   def name = "RichTextEditor"
 
   override def menus = List(Menu.i("TEXTILE_PREVIEW") / "textile" >> Hidden)
+
+  override def init {
+    ResourceServer.allow {
+      case "rte" :: _ => true
+    }
+  }
 }
--- src/main/scala/de/sbradl/liftedcontent/rte/snippet/RichTextEditor.scala
+++ src/main/scala/de/sbradl/liftedcontent/rte/snippet/RichTextEditor.scala
@@ -6,12 +6,12 @@ class RichTextEditor {
 
   def textile = {
     <head>
-      <link rel="stylesheet" type="text/css" href="/markitup/skins/markitup/style.css"/>
-      <link rel="stylesheet" type="text/css" href="/markitup/style.css"/>
+      <link rel="stylesheet" type="text/css" href="/classpath/rte/markitup/skins/markitup/style.css"/>
+      <link rel="stylesheet" type="text/css" href="/classpath/rte/markitup/style.css"/>
     </head>
     <tail>
-      <script src="/markitup/jquery.markitup.js"></script>
-      <script src="/markitup/set.js"></script>
+      <script src="/classpath/rte/markitup/jquery.markitup.js"></script>
+      <script src="/classpath/rte/markitup/set.js"></script>
       <script language="javascript">
 		mySettings.previewParserPath = '{S.contextPath + "/textile"}';
         $('textarea').markItUp(mySettings);
@@ -20,18 +20,18 @@ class RichTextEditor {
   }
 
   def render = <head>
-                 <link rel="stylesheet" type="text/css" href="/css/sh_style.css"/>
-                 <link rel="stylesheet" type="text/css" href="/css/sh_ide-eclipse.min.css"/>
+                 <link rel="stylesheet" type="text/css" href="/classpath/rte/css/sh_style.css"/>
+                 <link rel="stylesheet" type="text/css" href="/classpath/rte/css/sh_ide-eclipse.min.css"/>
                </head>
                <div id="editor_controls"></div>
                <tail>
                  <script language="javascript">
                    var CONTEXT_ROOT = "{ S.contextPath }";
                  </script>
-                 <script src="/js/RichTextEditor.js"></script>
-                 <script src="/js/sh_main.min.js"></script>
-                 <script src="/js/sh_xml.min.js"></script>
-                 <script src="/js/sh_java.min.js"></script>
+                 <script src="/classpath/rte/js/RichTextEditor.js"></script>
+                 <script src="/classpath/rte/js/sh_main.min.js"></script>
+                 <script src="/classpath/rte/js/sh_xml.min.js"></script>
+                 <script src="/classpath/rte/js/sh_java.min.js"></script>
                  <script language="javascript">
                    jQuery(document).ready(function() {{
   					  enableDesignMode();
